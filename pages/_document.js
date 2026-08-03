408cf1881bd8b1d186c02e86b52ab5841f828228
shiv-rishinarayan <shivam.rishinarayansingh@gmail.com>
2026-08-03 23:37:12 +0530

    Add Google Analytics tag (G-4BQGPF7L3J) to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,18 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/png" href="/Gold_and_silver_thumbnail.png" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4BQGPF7L3J" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4BQGPF7L3J');
+            `,
+          }}
+        />
       </Head>
       <body className="antialiased">
         <Main />
